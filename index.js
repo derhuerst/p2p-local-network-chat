3abe150e13953811a0480f77e6074408ef76f847
Jannis R <mail@jannisr.de>
2017-10-06 01:52:35 +0200

    announce server, connect to others

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
 'use strict'
 
-// todo
+const createServer = require('./lib/create-server')
+const announceSelf = require('./lib/announce-self')
+const findPeers = require('./lib/find-peers')
+const id = require('./lib/id')
+
+const showError = (err) => {
+	console.error(err)
+	process.exit(1)
+}
+
+createServer((err, server, address, port) => {
+	if (err) return showError(err)
+	console.error(id, address, port)
+
+	announceSelf(id, address, port, (err) => {
+		if (err) return showError(err)
+		console.error('announced')
+
+		const peers = findPeers()
+		peers.on('peer', (peer, connection) => {
+			const s = createReplicationStream()
+			s.pipe(connection).pipe(s)
+		})
+	})
+})
