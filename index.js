d25e6a78df84aafbffa3e11f307273951bc71c7e
Jannis R <mail@jannisr.de>
2017-10-07 21:56:13 +0200

    use really-basic-chat-ui

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 'use strict'
 
+const createUI = require('really-basic-chat-ui')
+
 const createModel = require('./lib/create-model')
 const findPeers = require('./lib/find-peers')
 const id = require('./lib/id')
@@ -11,12 +13,22 @@ const showError = (err) => {
 
 const model = createModel()
 
-const replicate = (connection) => {
-	const s = model.createStream()
-	s.pipe(connection).pipe(s)
+const send = (content) => {
+	model.push({from: id, when: Date.now(), content})
 }
 
-findPeers((peer) => {
-	console.error('peer', peer.id)
-	replicate(peer)
+const nrOfPeers = findPeers((peer) => {
+	// replicate
+	const s = model.createStream()
+	s.pipe(peer).pipe(s)
+
+	// greet
+	model.push({from: id, when: Date.now(), content: `Hey ${peer.id}!`})
 })
+
+const render = createUI(send)
+const rerender = () => {
+	const messages = model.toJSON()
+	render(true, messages, null, nrOfPeers())
+}
+model.on('update', rerender)
